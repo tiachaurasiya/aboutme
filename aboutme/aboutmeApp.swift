755abba7c0871fbaea3154fd69b76d0da7c2e33a
Tia Chaurasiya <tiachh73@gmail.com>
2024-06-21 10:36:19 -0400

    Initial Commit

---
+++ aboutme/aboutmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutmeApp.swift
+//  aboutme
+//
+//  Created by tia on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct aboutmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
