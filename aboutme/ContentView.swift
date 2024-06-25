5aaf9ce5cefc65f14ae83c8f7c9cb9f9b0c8ec51
Tia Chaurasiya <tiachh73@gmail.com>
2024-06-25 11:33:50 -0400

    updated

--- aboutme/ContentView.swift
+++ aboutme/ContentView.swift
@@ -15,19 +15,65 @@ struct ContentView: View {
 
         ZStack {
             Color(red: 0.4902, green: 0.6078, blue: 0.4627)
+                .ignoresSafeArea()
             VStack(alignment: .center) {
                
                 Text("TIA CHAURASIYA")
+                
                     .font(.title)
                     .fontWeight(.bold)
-                    
+                    .foregroundColor(Color.white)
+                Spacer()
+                Text("🎧☁️🕶️ ")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Spacer()
+                Spacer()
+            
+               
+               
+              
+            
+                VStack(spacing: 0.0) {
+
+                        Image("shoes")
+                                .resizable()
+                        .aspectRatio(contentMode: .fit)
+                                .cornerRadius(15)
+
+                        }
+                VStack {
+
+                        Image("jump")
+                                .resizable()
+                        .aspectRatio(contentMode: .fit)
+                                .cornerRadius(15)
+
+                        }
+                VStack {
+
+                        Image("cat")
+                                .resizable()
+                        .aspectRatio(contentMode: .fit)
+                                .cornerRadius(15)
+
+                        }
+                Spacer()
+                
+                Text("hi! i'm tia. i love stylish shoes, the ocean, and cats ☼ ")
+                    .font(.headline)
+                .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+               
         }
-            .ignoresSafeArea()
-        
+            
         }
+       
+
     }
 }
-   
 #Preview {
     ContentView()
 }
+
